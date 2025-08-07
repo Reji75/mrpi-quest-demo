0bb4add0e7ae41834f2111bab771f46a934ffd28
Reji75 <ipeker75@yahoo.com>
2025-08-07 16:08:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,46 +4,49 @@ const ctx = canvas.getContext("2d");
 canvas.width = 320;
 canvas.height = 320;
 
+const playerImage = new Image();
+playerImage.src = "mrpi_sprite_64x64.png"; // Make sure this image is in your project root
+
 const player = {
   x: 150,
   y: 150,
-  size: 20,
-  color: "blue",
-  speed: 5,
+  width: 32,
+  height: 32,
+  speed: 2,
 };
 
-function drawPlayer() {
-  ctx.fillStyle = player.color;
-  ctx.fillRect(player.x, player.y, player.size, player.size);
+let keys = {};
+
+document.addEventListener("keydown", (e) => {
+  keys[e.key] = true;
+});
+
+document.addEventListener("keyup", (e) => {
+  keys[e.key] = false;
+});
+
+function updatePlayerPosition() {
+  if (keys["ArrowUp"] && player.y > 0) player.y -= player.speed;
+  if (keys["ArrowDown"] && player.y + player.height < canvas.height) player.y += player.speed;
+  if (keys["ArrowLeft"] && player.x > 0) player.x -= player.speed;
+  if (keys["ArrowRight"] && player.x + player.width < canvas.width) player.x += player.speed;
 }
 
 function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function update() {
-  clearCanvas();
-  drawPlayer();
-  requestAnimationFrame(update);
+function drawPlayer() {
+  ctx.drawImage(playerImage, player.x, player.y, player.width, player.height);
 }
 
-// Listen for arrow key presses
-document.addEventListener("keydown", function (event) {
-  switch (event.key) {
-    case "ArrowUp":
-      player.y -= player.speed;
-      break;
-    case "ArrowDown":
-      player.y += player.speed;
-      break;
-    case "ArrowLeft":
-      player.x -= player.speed;
-      break;
-    case "ArrowRight":
-      player.x += player.speed;
-      break;
-  }
-});
+function gameLoop() {
+  updatePlayerPosition();
+  clearCanvas();
+  drawPlayer();
+  requestAnimationFrame(gameLoop);
+}
 
-// Start the game loop
-update();
+playerImage.onload = function () {
+  gameLoop();
+};
